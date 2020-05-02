c73b3b15a1677eb42b880a1c9b5b6bb26af6cdb6
kuttimani <kuttimani@gmail.com>
2020-05-02 17:38:06 +0100

    changes

--- bootstrapjobs/testjobs.groovy
+++ bootstrapjobs/testjobs.groovy
@@ -5,8 +5,27 @@ folder('testjobs'){
 pipelineJob('testjobs/hello-world'){
     definition {
         displayName('Build test job')
+        logrotator {
+          daysToKeep(5)
+          numToKeep(5)
+        }
         throttleConcurrentBuilds{
           maxTotal(1)
+        }
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/kuttimani/k8-jenkins-eks-fargate-elk.git')
+
+                    }
+                    branches('master')
+                    scriptPath('testpath')
+                    extensions {
+                      wipeOutWorkspace()
+                    }
+                }
+            }
         }
     }
 }
