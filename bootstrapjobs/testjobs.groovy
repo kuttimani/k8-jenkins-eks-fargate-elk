5efadb06ac1eebede809899c845da7b274c59be2
kuttimani <kuttimani@gmail.com>
2020-05-02 17:16:41 +0100

    changes

--- bootstrapjobs/testjobs.groovy
+++ bootstrapjobs/testjobs.groovy
@@ -5,10 +5,6 @@ folder('testjobs'){
 pipelineJob('testjobs/hello-world'){
     definition {
         displayName('Build test job')
-        logrotator {
-          daysToKeep(5)
-          numToKeep(5)
-        }
         throttleConcurrentBuilds{
           maxTotal(1)
         }
