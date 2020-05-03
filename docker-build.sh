49c58965d3fe0a18b48270e7390576deaf75e49b
kuttimani <kuttimani@gmail.com>
2020-05-03 17:29:54 +0100

    .

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,4 @@ rm -rf /tmp/dockertmp
 mkdir /tmp/dockertmp
 cp jenkinsConfigs/executors.groovy /tmp/dockertmp/
 cp plugins.txt /tmp/dockertmp/
-docker build -t kuttimani/jenkins-k8-image:0.5 -f ./Dockerfile /tmp/dockertmp
+docker build -t kuttimani/jenkins-k8-image:0.6 -f ./Dockerfile /tmp/dockertmp
